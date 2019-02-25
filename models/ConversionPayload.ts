fb3127fe3fefed68e23df8467199d4fbdf01f2b4
inoabrian <brianmust1@gmail.com>
2019-02-25 15:21:37 -0500

    Updates to interfaces

--- models/ConversionPayload.ts
+++ models/ConversionPayload.ts
@@ -2,5 +2,5 @@ export interface ConversionPayload {
     to: Array<string> | string;
     from: Array<string> | string;
     value: string;
-    formatted: string;
+    convertedValue: number;
 };
--- models/ExchangeResponse.ts
+++ models/ExchangeResponse.ts
@@ -1,14 +1,45 @@
-export interface Sender {
-    name: string;
+
+export interface Attributes {
+    version: string;
+    encoding: string;
 }
 
-export interface Cube3 {
+export interface Declaration {
+    _attributes: Attributes;
+}
+
+export interface Attributes2 {
+    "xmlns:gesmes": string;
+    xmlns: string;
+}
+
+export interface GesmesSubject {
+    _text: string;
+}
+
+export interface GesmesName {
+    _text: string;
+}
+
+export interface GesmesSender {
+    "gesmes:name": GesmesName;
+}
+
+export interface Attributes3 {
+    time: string;
+}
+
+export interface Attributes4 {
     currency: string;
     rate: string;
 }
 
+export interface Cube3 {
+    _attributes: Attributes4;
+}
+
 export interface Cube2 {
-    time: string;
+    _attributes: Attributes3;
     Cube: Cube3[];
 }
 
@@ -16,8 +47,14 @@ export interface Cube {
     Cube: Cube2;
 }
 
-export interface ExchangeResponse {
-    subject: string;
-    Sender: Sender;
+export interface GesmesEnvelope {
+    _attributes: Attributes2;
+    "gesmes:subject": GesmesSubject;
+    "gesmes:Sender": GesmesSender;
     Cube: Cube;
+}
+
+export interface ExchangeResponse {
+    _declaration: Declaration;
+    "gesmes:Envelope" : GesmesEnvelope;
 }
